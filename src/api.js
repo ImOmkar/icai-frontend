4d173485f151d5c3be828ca5f807158820d8c37d
ImOmkar <omkar211196@gmail.com>
2025-11-15 15:45:07 +0530

    package removed

--- src/api.js
+++ src/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 const API = axios.create({
   baseURL: 'https://icai-frontend.vercel.app',
-  // baseURL: 'http://localhost:4000',
   timeout: 5000
 });
 export default API;
